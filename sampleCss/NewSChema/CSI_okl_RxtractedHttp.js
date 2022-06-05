b6d2e51fedaa5b731b571338667517b6635b849f
Pranav Shrikhande <pranav.shrikhande@gmail.com>
2022-06-05 16:50:59 -0400

    ok

---
+++ sampleCss/NewSChema/CSI_okl_RxtractedHttp.js
@@ -0,0 +1,424 @@
+{
+    "computedDataID":"",
+    "auditUploadID" : "",  
+    "extractedDataID" : "",
+    "createTimeStamp" : "",
+    "documentStatus":  "",
+    "lastUpdatedBy"   : "",
+    "lastUpdatedDate":  "",
+    "updateCount": "",
+    "documentType": "",
+    "documentID": "",             
+    "storeID": "",
+    "state":"",
+    "division":"",
+    "district":"",
+    "region":"",
+    "pageNumber": "",
+    "formConfidence": "",
+    "formCompliantStatus": "",  
+    "form_level":[
+      {
+            "itemId": 1,
+            "value": "Store #",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 2,
+            "value": "Date Conducted",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"N",
+            "extractionOptions": [],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 3,
+            "value": "Address",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 4,
+            "value": "Phone Number",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 4,
+            "value": "Phone Number",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 5,
+            "value": "Pharmacy Number",
+            "confidenceScore": "",
+            "extractionResult": ""   , 
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 6,
+            "value": "When Conducted",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                 
+                {
+                    "value": "Close",
+                    "display_value": "Cloes"
+                },
+                {
+                    "value": "Start of Business",
+                    "display_value": "Start of Business"
+                },
+                {
+                    "value": "24 Hour Pharmacy",
+                    "display_value": "24 Hour Pharmacy"
+                }
+                
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 7,
+            "value": "Inventory Reason",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                 
+                {
+                    "value": "Ownership Change",
+                    "display_value": "Ownership Change"
+                },
+				{
+                    "value": "Change in PIC",
+                    "display_value": "Change in PIC"
+                },
+                {
+                    "value": "Pharmacy Closing",
+                    "display_value": "Pharmacy Closing"
+                },
+                {
+                    "value": "Annual Inventory for Renewal",
+                    "display_value": "Annual Inventory for Renewal"
+                },
+                
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 8 ,
+            "value": "Effective Date",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+            "itemId": 9,
+            "value": "Change in PIC",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Incoming PIC",
+                    "display_value": "Incoming PIC"
+                },
+                {
+                    "value": "Outgoing PIC",
+                    "display_value": "Outgoing PIC"
+                },
+                {
+                    "value": "None",
+                    "display_value": "None"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        },
+		{
+			"itemId": 10,
+            "value": "Outgoing PIC",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Incoming PIC",
+                    "display_value": "Incoming PIC"
+                },
+                {
+                    "value": "Outgoing PIC",
+                    "display_value": "Outgoing PIC"
+                },
+                {
+                    "value": "None",
+                    "display_value": "None"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+		},
+		{
+            "itemId": 10,
+            "value": "Signature of RPh",
+            "confidenceScore": "",
+            "extractionResult": "",
+            "extractionIsDropdown":"Y",
+            "extractionOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Detected"
+                },
+                {
+                    "value": "N",
+                    "display_value": "Not Detected"
+                }
+            ],
+            "complianceScore": "",
+            "complianceOptions": [
+                {
+                    "value": "Y",
+                    "display_value": "Y"
+                },
+                {
+                    "value": "N",
+                    "display_value": "N"
+                },
+                {
+                    "value": "I",
+                    "display_value": "I"
+                }
+            ]
+        }
+	
+    ],
+    "line_items": []
+}
+
